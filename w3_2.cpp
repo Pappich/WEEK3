15ad326c5be3398acb02aa94e79a07c3c6170552
Pappich Pongpattanawut <88368170+Pappich@users.noreply.github.com>
2021-09-03 01:59:35 +0700

    Update w3_2.cpp

--- w3_2.cpp
+++ w3_2.cpp
@@ -2,7 +2,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 int main()
 {
-	int n,i,j,a,b,c,x;
+    int n,i,j,a,b,c,x;
     printf("Enter your number :");
     scanf("%d",&n);
     a = (2*n)-1;
@@ -12,25 +12,19 @@ int main()
         b = a;
 	}
 	for(i=n-1;i>=1;i--)
-    {
+    	{
         for(j=1;j<=b;j++)
 		{
             if(j<=i)
             {
                 printf(" ");
-                c  = c+1;
             }
             else
         	{
                 printf("*");
             }
         }
-        for(x=1;x<=c;x++)
-		{
-            printf(" ");
-        }
-   	c=0;
-        b=b+1;
+    	b=b+1;
   	printf("\n");
 	}
 	for(i=1;i<=(2*n)-1;i++)
